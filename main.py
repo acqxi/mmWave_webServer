f0fdfa7e89768217b78a2d97175e88d4e5f20dbd
awrgto <minecraftpetim@gmail.com>
2020-08-20 11:05:14 +0800

    try1

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-from flask import Flask, render_template, request
-from random import choice
+import json
 
-web_site = Flask(__name__)
+from flask import Flask, request
 
-number_list = [
-	100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415,
-	416, 417, 418, 421, 422, 423, 424, 425, 426,
-	429, 431, 444, 450, 451, 500, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599
-]
+app = Flask( __name__ )
 
-@web_site.route('/')
-def index():
-	return render_template('index.html')
 
-@web_site.route('/user/', defaults={'username': None})
-@web_site.route('/user/<username>')
-def generate_user(username):
-	if not username:
-		username = request.args.get('username')
+@app.route( '/', methods=[ 'GET' ] )
+def hello_world():
+    return 'Hello World!'
 
-	if not username:
-		return 'Sorry error something, malformed request.'
 
-	return render_template('personal_user.html', user=username)
+@app.route( '/customerupdate', methods=[ 'GET', 'POST' ] )
+def customerupdate():
+    posted_file = str( request.files[ 'document' ].read(), 'utf-8' )
+    posted_data = json.load( request.files[ 'datas' ] )
+    print( posted_file )
+    print( posted_data )
+    return '{}\n{}\n'.format( posted_file, posted_data )
 
-@web_site.route('/page')
-def random_page():
-  return render_template('page.html', code=choice(number_list))
 
-web_site.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run( host='0.0.0.0', port=8080 )
